14327be986910485ed17b8eec7bf20d0c26a1a8b
Ahmad Naser Almuhtar <an@rechenwerk.pro>
2023-12-23 22:11:32 +0100

    Update Code

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAllBooks = async () => {
@@ -26,6 +27,11 @@ const Books = () => {
     }
   };
 
+  const handleUpdate = (id) => {
+    // Navigate to the update page with the book id
+    navigate(`/update/${id}`);
+  };
+
   return (
     <>
       <h1>Ahmad's Book Shop</h1>
@@ -39,9 +45,9 @@ const Books = () => {
             <button className="delete" onClick={() => handleDelete(book.id)}>
               Delete
             </button>
-            <Link to={`/update/${book.id}`}>
-              <button className="update">Update</button>
-            </Link>
+            <button className="update" onClick={() => handleUpdate(book.id)}>
+              Update
+            </button>
           </div>
         ))}
       </div>
